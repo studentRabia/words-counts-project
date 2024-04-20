5b261c46b700c3178993f7d9644a79ff81fe4864
studentRabia <164480298+studentRabia@users.noreply.github.com>
2024-04-20 23:29:51 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# words-counts-project" 
---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.cyanBright.bold(`\n\t\t" Word counter Applicition "`));
+console.log(chalk.cyanBright('='.repeat(60)));
+
+let answers:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter  A Sentence."
+    }
+]);
+
+//triming the sentence & spit ting inito words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+
+//analysis of user input setence
+
+console.log(chalk.cyanBright("=".repeat(60)));
+console.log(chalk.bold("\n- Sentence Word: "));
+console.log(words);
+console.log(chalk.bold(`\n - Word Counts: '${chalk.bold.redBright(words.length)}'`));
+console.log(chalk.cyanBright("=".repeat(60)));
+
+
